c9255b6a26d5d5813e5e2c8d8da820001089847a
star-u <star1998star@163.com>
2021-03-23 20:03:42 +0800

    accuracy

--- TestPro.py
+++ TestPro.py
@@ -26,24 +26,29 @@ from util import *
 test_dataset = {"1": "MUTAG", "2": "PROTEINS_full", "3": "PTC_FM", "4": "PTC_FR", "5": "PTC_MM", "6": "PTC_MR",
                 "7": "NCI109", "8": "NCI1"}
 
-for key, value in test_dataset.items():
-    dataset = fetch_dataset(value, verbose=False)
-    G, y = dataset.data, dataset.target
-    # print(len(G))
-    # print(G[0][1])
-    # Splits the dataset into a training and a test set
-    G_train, G_test, y_train, y_test = train_test_split(G, y, test_size=0.1, random_state=42)
-    # print((np.array(G_train)).shape)
-    # Uses the Weisfeiler-Lehman subtree kernel to generate the kernel matrices
-    gk = WeisfeilerLehman(n_iter=3, base_graph_kernel=VertexHistogram, normalize=True)
-    K_train = gk.fit_transform(G_train)
-    K_test = gk.transform(G_test)
+f = open('Accuracy.txt', 'a')
+for iter_number in [2, 10, 20, 30, 40, 50]:
+    f.write("-----"+str(iter_number)+"----------------\n")
+    for key, value in test_dataset.items():
+        dataset = fetch_dataset(value, verbose=False)
+        G, y = dataset.data, dataset.target
+        # print(len(G))
+        # print(G[0][1])
+        # Splits the dataset into a training and a test set
+        G_train, G_test, y_train, y_test = train_test_split(G, y, test_size=0.1, random_state=42)
+        # print((np.array(G_train)).shape)
+        # Uses the Weisfeiler-Lehman subtree kernel to generate the kernel matrices 2,10,20
+        gk = WeisfeilerLehman(n_iter=iter_number, base_graph_kernel=VertexHistogram, normalize=True)
+        K_train = gk.fit_transform(G_train)
+        K_test = gk.transform(G_test)
 
-    # Uses the SVM classifier to perform classification
-    clf = SVC(kernel="precomputed")
-    clf.fit(K_train, y_train)
-    y_pred = clf.predict(K_test)
+        # Uses the SVM classifier to perform classification
+        clf = SVC(kernel="precomputed")
+        clf.fit(K_train, y_train)
+        y_pred = clf.predict(K_test)
 
-    # Computes and prints the classification accuracy
-    acc = accuracy_score(y_test, y_pred)
-    print("{}: {}'s Accuracy: ".format(key, value), str(round(acc * 100, 2)) + "%")
+        # Computes and prints the classification accuracy
+        acc = accuracy_score(y_test, y_pred)
+
+        f.write(str(key)+": "+str(value)+"s Accuracy: "+str(round(acc * 100, 2)) + "%\n")
+f.close()
